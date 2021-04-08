1c5de435c9e44e02e4ee5c295244e5071a7b1bab
aldy96 <aldyfernando74@gmail.com>
2021-04-08 11:55:45 +0700

    baru

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import NavBar from './component/NavBar';
 
 export default function App() {
     return(
-        <div>asdvsgsdvsa</div>
+        <div>
+            <NavBar/>
+        </div>
     );
 };
---
+++ src/component/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function() { 
+    return(
+        <div>
+            <nav class="navigasi">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    )
+ }
